b0433dccc3178209242f76102f61e4f10c9432aa
Jesus Aguilera <programacionOnly@gmail.com>
2021-03-10 09:22:50 -0600

    mejorando el singleStorage

--- database/singleStorage.js
+++ database/singleStorage.js
@@ -1,23 +1,26 @@
 import React, {useReducer,useState,useEffect} from 'react';
 import { Text,Dimensions,TouchableOpacity,StyleSheet,ScrollView } from 'react-native';
 import { View } from 'react-native-animatable';
-import Notifications from '../model/Notifications';
 import { ListItem } from 'react-native-elements'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { set } from 'react-native-reanimated';
 
 const SingleStorage = () => {
 
-    const initialState = {notif: Notifications};
+    //const initialState = {notif: Notifications};
 
-    const [id, setId] = useState('555');
-    const [title, setTitle] = useState('Prueba 1');
-    const [details, setDetails] = useState('Detalles de la prueba 1');
+    const [id, setId] = useState(0);
+    const [title, setTitle] = useState('');
+    const [details, setDetails] = useState('');
     const [data, setData] = useState([]);
     const [toggle, setToggle] = useState('true'); //no hace nada solo lo uso para actualizar y que se actualize el useEffect
 
 
 
-    const guardarNotificaciones=async()=>{
+    const guardarNotificaciones=async(idN,titleN,detailsN)=>{
+        setId(idN);
+        setTitle(titleN);
+        setDetails(detailsN);
         let infoNotification = {
             id,
             title,
@@ -36,27 +39,31 @@ const SingleStorage = () => {
         if (storedData === null) {
           // save
             await AsyncStorage.setItem('DB_Notifications', JSON.stringify(arrData));
+            
         } else {
             newData = [...storedDataParsed, infoNotification];
             await AsyncStorage.setItem('DB_Notifications', JSON.stringify(newData));
+            
         }
-        setToggle(false);
+        
     }
     
     
-    const recuperarDatos = async () => {
+    const traerDB_Notification = async () => {
         try {
             const valueString = await AsyncStorage.getItem('DB_Notifications');
             const value = JSON.parse(valueString);
             
             setData(value);
             console.log(value);
+            return value;
         } catch (error) {
             console.log(error);
+            return 'error'
         }
     };
 
-    const clearData = async (id) => {
+    const eliminarNotificacion = async (id) => {
         if (data !== null) {
             const newData = data.filter((_, index) => index !== id);
             setData(newData);
@@ -64,6 +71,28 @@ const SingleStorage = () => {
         }
     };
 
+    const notificacionesExistencia=()=>{
+        if (storedData === null) {
+            setToggle(false);
+            console.log('No existen Notifiaciones almacenadas')
+            return false;
+        }else{
+            setToggle(true);
+            console.log('Existen notificaciones')
+            return true;
+        }
+    }
+
+    const cantidadNotificaciones=()=>{
+        const numero_Notifi=data;
+        const cantidad=Object.keys(numero_Notifi).length;
+        console.log(Object.keys(numero_Notifi));
+        return cantidad;
+    }
+
+    const eliminarTodasNotificaciones=()=>{
+        AsyncStorage.clear();
+    }
 
     // const crud = (state, action) => { 
     //     switch (action.type) {
@@ -107,25 +136,35 @@ const SingleStorage = () => {
     // };
 
     useEffect(() => {
-        
-        recuperarDatos();
+        //eliminarTodasNotificaciones()
+        traerDB_Notification();// este se encarga de actualizar y returnar la base de datos de notificaciones
     },[]);
 
     return(
         <ScrollView>
             <TouchableOpacity
                     onPress={() => {
-                        guardarNotificaciones()
+                        guardarNotificaciones(2,"notifi 0",'detalle 0')
+                        console.log(cantidadNotificaciones());
                     }}style={[styles.signIn,{borderColor: "#2096BA",borderWidth: 1,marginTop: 12,marginLeft: 10,flexDirection: "column",},]}
                     >
                         <Text style={[styles.textSign,{color: "#0064A2",},]}>
                             Agregar Notificacion
                         </Text>
             </TouchableOpacity>
-            { data && (data.map((NotificationItem, index) => (
+            <TouchableOpacity
+                    onPress={() => {
+                        eliminarNotificacion(0);
+                    }} style={[styles.signIn,{borderColor: "#2096BA",borderWidth: 1,marginTop: 12,marginLeft: 10,flexDirection: "column",},]}
+                    >
+                        <Text style={[styles.textSign,{color: "#0064A2",},]}>
+                            Eliminar Notificacion
+                        </Text>
+            </TouchableOpacity>
+            { data &&  (data.map((NotificationItem, index) => (
                             <ListItem key={index} bottomDivider>
                                 <ListItem.Content>
-                                    <ListItem.Title>key: {NotificationItem.index}</ListItem.Title>
+                                    <ListItem.Title>key: {NotificationItem.id}</ListItem.Title>
                                     <ListItem.Subtitle>title: {NotificationItem.title}</ListItem.Subtitle>
                                     <ListItem.Subtitle>details: {NotificationItem.details}</ListItem.Subtitle>
                                 </ListItem.Content>
@@ -143,6 +182,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 5,
+        marginBottom: 50
     },
     textSign: {
         fontSize: 14,
